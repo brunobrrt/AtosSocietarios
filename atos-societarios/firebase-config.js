ed986b782db11fed6100c751f58463dc15177f29
brunobrrt <brunolol_10@hotmail.com>
2026-04-01 15:49:55 -0300

    Update Firebase configuration for Atos Societarios project

--- atos-societarios/firebase-config.js
+++ atos-societarios/firebase-config.js
@@ -1,12 +1,12 @@
 // ===== CONFIGURAÇÃO DO FIREBASE =====
-// Projeto: contabilidade-b568c
+// Projeto: atossocietarios-cc48d
 
 window.firebaseConfig = {
-    apiKey: "AIzaSyBloLCULnravGmDtTKaTjpeWaKzt_6LU_c",
-    authDomain: "contabilidade-b568c.firebaseapp.com",
-    projectId: "contabilidade-b568c",
-    storageBucket: "contabilidade-b568c.firebasestorage.app",
-    messagingSenderId: "1048916585304",
-    appId: "1:1048916585304:web:35eb47b58d0d0b6ce6932d",
-    measurementId: "G-81T555MS7L"
+    apiKey: "AIzaSyDv390Q31ehf9SBdlFQBrmvsByzi2BxOYY",
+    authDomain: "atossocietarios-cc48d.firebaseapp.com",
+    projectId: "atossocietarios-cc48d",
+    storageBucket: "atossocietarios-cc48d.firebasestorage.app",
+    messagingSenderId: "1063285830045",
+    appId: "1:1063285830045:web:76f4d81224bed786d3ff74",
+    measurementId: "G-RT2ZZ265ZY"
 };
